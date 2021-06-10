7d2d1fb4b3e81566bd4a09c8c6c303d42dcdd1d3
kamiyagi <webmaster@dojin-music.info>
2021-06-11 01:05:16 +0900

    M3のサークルリストからスペース一覧を出すやつ

---
+++ crawler_m3/crawler.py
@@ -0,0 +1,88 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+res = requests.get('https://www.m3net.jp/attendance/circle2021sR.php')
+
+# レスポンスの HTML から BeautifulSoup オブジェクトを作る
+soup = BeautifulSoup(res.text, 'html.parser')
+
+# title タグの文字列を取得する
+tables = soup.select(".tblCircleList")
+
+event_id = 5
+
+values1 = []
+for data in tables:
+    tr = data.find_all("tr")
+    for row in tr:
+        space = row.find(class_="left").get_text(strip=True)
+        link = row.find("li").find("a")
+
+        if (link):
+            name = link.get_text(strip=True)
+            url = link['href']
+        else:
+            link = row.find("li")
+            name = link.get_text(strip=True)
+            url = ""
+
+
+        # 現行仕様はWEBのスペースの情報も併記されている
+        single_space = re.search(r'^[A-Za-zあ-んア-ン]-[0-9]+', space).group()
+        # print(single_space)
+        # print(name)
+        # print(url)
+
+        sp = single_space.split("-")
+        space_head = "'" + sp[0] + "'" if single_space else "null"
+        space_body = "'" + sp[1] + "'" if single_space else "null"
+        text = "(null, " + str(event_id) + ", " + space_head + ", " + space_body + ", '" + name + "', " + ("null" if url == "" else "'" + url + "'") + ")"
+        values1.append(text)
+print("INSERT INTO dojin_event_space` VALUES")
+print(','.join(values1) + ";")
+
+
+
+
+res = requests.get('https://www.m3net.jp/attendance/circle2021sW.php')
+
+# レスポンスの HTML から BeautifulSoup オブジェクトを作る
+soup = BeautifulSoup(res.text, 'html.parser')
+
+# title タグの文字列を取得する
+tables = soup.select(".tblCircleList")
+
+event_id = 6
+
+values1 = []
+for data in tables:
+    tr = data.find_all("tr")
+    for row in tr:
+        space = row.find(class_="left").get_text(strip=True)
+        link = row.find("li").find("a")
+
+        if (link):
+            name = link.get_text(strip=True)
+            url = link['href']
+        else:
+            link = row.find("li")
+            name = link.get_text(strip=True)
+            url = ""
+
+        # 現行仕様はWEBのスペースの情報も併記されている
+        single_space = re.search(r'[一-鿐]-[0-9]+$', space).group()
+        # print(single_space)
+        # print(name)
+        # print(url)
+
+        sp = single_space.split("-")
+        space_head = "'" + sp[0] + "'" if single_space else "null"
+        space_body = "'" + sp[1] + "'" if single_space else "null"
+        text = "(null, " + str(event_id) + ", " + space_head + ", " + space_body + ", '" + name + "', " + ("null" if url == "" else "'" + url + "'") + ")"
+        values1.append(text)
+print("INSERT INTO dojin_event_space` VALUES")
+print(','.join(values1) + ";")
+
+
+
